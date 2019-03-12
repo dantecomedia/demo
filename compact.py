ec3eb0aef56214ddf017aac9e4a1ca36aa6b0830
rahul <rahul.cse.ccu@gmail.com>
2019-03-13 01:02:16 +0530

    kernel_initializer

--- compact.py
+++ compact.py
@@ -9,7 +9,7 @@ feed_forward=[]
 class feedforward:
     
     
-    def model(self,nos_layers,model_type,input_dim, output_dim,hidden_dim, hid_act,out_act,init,loss,metrics,optimizer, dropout):
+    def model(self,nos_layers,model_type,input_dim, output_dim,hidden_dim, hid_act,out_act,kernel_initializer,loss,metrics,optimizer, dropout):
         self.nos_layers=nos_layers  #NUMBER OF LAYERS
         self.mode_type=model_type   #SEQUENTIAL / FUNCTIONAL
         self.input_dim=input_dim  # INPUT DIMENSION
@@ -17,7 +17,7 @@ class feedforward:
         self.output_dim=output_dim      #OUTPUT DIMENSION
         self.hid_act=hid_act        #HIDDEN ACTIVATION FUNCTION
         self.out_act=out_act        #OUTPUT ACTIVATION FUNCTION
-        self.init=init            #INITAILIZATION
+        self.kernel_initializer=kernel_initializer         #kernel_kernel_initializerializerAILIZATION
         self.loss=loss              #LOSS FUNCTION
         self.metrics=metrics        #METRICS LIST
         self.dropout=dropout        #DROPOUT
@@ -27,13 +27,13 @@ class feedforward:
         if (model_type=='Sequential'):
             from keras.models import Sequential
             models=Sequential()         #calling model type
-        models.add(Dense(output_dim = hidden_dim, kernel_initializer = init , activation = hid_act, input_dim = input_dim)) #first hidden layer
+        models.add(Dense(output_dim = hidden_dim, kernel_initializer = kernel_initializer , activation = hid_act, input_dim = input_dim)) #first hidden layer
         models.add(Dropout(p=dropout))
                                     # Adding the second hidden layer
         for i in range(1,nos_layers-1):
-            models.add(Dense(output_dim =hidden_dim , kernel_initializer = init, activation = hid_act))
+            models.add(Dense(output_dim =hidden_dim , kernel_initializer = kernel_initializer, activation = hid_act))
             models.add(Dropout(p=dropout))
-        models.add(Dense(output_dim = output_dim, kernel_initializer = init, activation = out_act))
+        models.add(Dense(output_dim = output_dim, kernel_initializer = kernel_initializer, activation = out_act))
         models.compile(optimizer=optimizer,loss=loss,metrics=metrics)
         feed_forward.append(models)
     
